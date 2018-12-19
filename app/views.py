a3a7115775e9de03c35bf8c71f08793af32cbf2a
totonunez <you@example.com>
2018-12-18 21:10:03 -0300

    lo que salga

--- app/views.py
+++ app/views.py
@@ -34,8 +34,8 @@ def index():
 										from involucrados group by id_evento) as jj) and choques.id_evento = kk.id_evento;"""
 
 		print sql
-		granchoque = cur.fetchall()
 		cur.execute(sql)
+		granchoque = cur.fetchall()
 		conn.commit()
 	except:
 		pass
